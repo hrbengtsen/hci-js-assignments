49eb6dd30a0610b88c2e13c05f044474250257d7
Mikkel Bengtsen <mikkelbengtsen@gmail.com>
2020-09-14 21:43:00 +0200

    added a bunch of styles + working test skeleton

--- Assignment 1/js/code.js
+++ Assignment 1/js/code.js
@@ -1,28 +1,101 @@
-const gridSize = 5;
-let gridElement = document.getElementById("grid");
+// Define grid & elements
+const gridSize = 6;
+const gridElement = document.getElementById("grid");
+const subTitleElement = document.getElementById("subtitle");
 
-function clicked() {
-    alert("found me");
+// Define test
+let testActive = false;
+const amountOfTrials = 10;
+let trialsLeft = amountOfTrials;
+
+// Define performance
+let reactionTimes = [];
+let lastTimeGridWasShown;
+
+
+function oddClicked() {
+    // Set random delay between 1000-3000 ms before creating new grid
+    let randomDelay = Math.floor(2e3 * Math.random() + 1e3);
+
+    removePreviousGrid(gridSize, gridSize);
+    subTitleElement.innerText = "Test in progress...";
+
+    // Calculate reaction time if test is active, otherwise begin test
+    if (testActive) {
+        let currentTime = new Date().getTime();
+        let deltaTime = currentTime - lastTimeGridWasShown;
+        reactionTimes.push(deltaTime);
+        trialsLeft--;
+    }
+    else {
+        // Test started
+        testActive = true;
+        trialsLeft = amountOfTrials;
+    }
+
+    // Create new grid after delay if there are more trials left, otherwise end test
+    if (trialsLeft > 0) {
+        setTimeout(() => createRandomGrid(gridSize, gridSize), randomDelay);
+    }
+    else {
+        // Test finished
+        testActive = false;
+        subTitleElement.innerText = "Test finished!";
+    }
 }
 
-function createInitialGrid(row, col) {
-    let oddObject = [Math.floor(row * Math.random()), Math.floor(col * Math.random())];
+function removePreviousGrid(row, col) {
+    for (let i = 0; i < row * col; i++) {
+        let item = document.getElementById("item-" + i);
+        gridElement.removeChild(item);
+    }
+}
 
-    for (let i = 0; i < row; i++) {
-        for (let j = 0; j < col; j++) {
-            let cell = document.createElement("div");
-            cell.innerText = "O";
-            gridElement.appendChild(cell).className = "grid-item";
+function createRandomGrid(row, col) {
+    // The odd object to identify
+    let oddItemIndex = Math.floor((row * col) * Math.random());
 
-            if (i === oddObject[0] && j === oddObject[1]) {
-                cell.innerText = "X";
-                cell.onclick = clicked;
-            }
+    for (let i = 0; i < row * col; i++) {
+        // Create grid item
+        let item = document.createElement("div");
+        gridElement.appendChild(item);
+        item.className = "grid-item";
+        item.id = "item-" + i;
+
+        // Create grid items content
+        let itemContent = document.createElement("div");
+        item.appendChild(itemContent);
+        itemContent.className = "grid-item-content";
+
+        // Mark the odd item
+        if (i === oddItemIndex) {
+            itemContent.className += " odd-item-content";
+            item.className += " odd-item";
+            item.onclick = oddClicked;
         }
     }
 
-    let numberOfObjectsToHide = Math.floor(row * Math.random());
-    // TODO: remove numberOfObjectsToHide from grid
+    // Hide random items for more random "grid"
+    let itemsToHide = [];
+    const numberOfItemsToHide = 8;
+    while (itemsToHide.length < numberOfItemsToHide) {
+        let randomIndex = Math.floor((row * col) * Math.random());
+
+        // Only hide if it isn't already and isn't the odd item
+        if (itemsToHide.indexOf(randomIndex) === -1 && randomIndex !== oddItemIndex) {
+            itemsToHide.push(randomIndex);
+            document.getElementById("item-" + randomIndex).style.visibility = "hidden";
+        }
+    }
+
+    // Remember when grid was shown
+    lastTimeGridWasShown = new Date().getTime();
+
+    // Update subtitle text
+    if (testActive) {
+        subTitleElement.innerText = "Find the odd item!";
+    }
 }
 
-window.onload = () => createInitialGrid(gridSize, gridSize);
+// Create grid onload
+window.onload = () => createRandomGrid(gridSize, gridSize);
