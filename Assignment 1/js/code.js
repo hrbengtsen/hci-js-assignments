fc0d0e26ee466383a55066db5c964d14a29b114c
Mikkel Bengtsen <mikkelbengtsen@gmail.com>
2020-09-16 12:12:49 +0200

    added all results & calculations + localstorage

--- Assignment 1/js/code.js
+++ Assignment 1/js/code.js
@@ -2,7 +2,7 @@
 const gridSize = 6;
 const gridElement = document.getElementById("grid");
 const subTitleElement = document.getElementById("subtitle");
-const trialsElement = document.getElementById("trials");
+const resultsElement = document.getElementById("results");
 
 // Define test
 let testActive = false;
@@ -13,28 +13,50 @@ let trialsLeft = amountOfTrials;
 let reactionTimes = [];
 let lastTimeGridWasShown;
 
+let bestOverallTime = JSON.parse(localStorage.getItem("best-overall-time"));
+let bestOverallMean = JSON.parse(localStorage.getItem("best-overall-mean"));
+
+// Set best-overall-time
+if (bestOverallTime < 100000) {
+    document.getElementById("best-overall-time").innerText = bestOverallTime + "ms";
+}
+else {
+    bestOverallTime = 100000;
+    localStorage.setItem("best-overall-time", JSON.stringify(bestOverallTime));
+    document.getElementById("best-overall-time").innerText = "none";
+}
+
+// Set best-overall-mean
+if (bestOverallMean < 100000) {
+    document.getElementById("best-overall-mean").innerText = bestOverallMean + "ms";
+}
+else {
+    bestOverallMean = 100000;
+    localStorage.setItem("best-overall-mean", JSON.stringify(bestOverallMean));
+    document.getElementById("best-overall-mean").innerText = "none";
+}
 
 function oddClicked() {
     // Set random delay between 1000-3000 ms before creating new grid
     let randomDelay = Math.floor(2e3 * Math.random() + 1e3);
-
-    removePreviousGrid(gridSize, gridSize);
-    subTitleElement.innerText = "Test in progress...";
 
     // Calculate reaction time if test is active, otherwise begin test
     if (testActive) {
         let currentTime = new Date().getTime();
         let deltaTime = currentTime - lastTimeGridWasShown;
         reactionTimes.push(deltaTime);
+
         trialsLeft--;
-        trialsElement.innerText = "Remaining tests: " + trialsLeft;
     }
     else {
         // Test started
         testActive = true;
         trialsLeft = amountOfTrials;
-        trialsElement.innerText = "Remaining tests: " + trialsLeft;
+        reactionTimes = [];
     }
+
+    removePreviousGrid(gridSize, gridSize);
+    subTitleElement.innerText = "Test in progress... " + trialsLeft + " remaining";
 
     // Create new grid after delay if there are more trials left, otherwise end test
     if (trialsLeft > 0) {
@@ -44,7 +66,104 @@ function oddClicked() {
         // Test finished
         testActive = false;
         subTitleElement.innerText = "Test finished!";
+        calculateAndShowResults();
     }
+}
+
+function calculateAndShowResults() {
+    // Calculate best & worst
+    let bestTime = Math.min(...reactionTimes);
+    let worstTime = Math.max(...reactionTimes);
+
+    // Update best-overall-time
+    if (bestTime < bestOverallTime) {
+        bestOverallTime = bestTime;
+        localStorage.setItem("best-overall-time", JSON.stringify(bestOverallTime));
+        document.getElementById("best-overall-time").innerText = bestOverallTime + "ms";
+    }
+
+    // Calculate mean
+    let meanDeltaTime = 0.0;
+    for (let i = 0; i < reactionTimes.length; i++) {
+        meanDeltaTime += reactionTimes[i];
+    }
+    meanDeltaTime = Math.round(meanDeltaTime / reactionTimes.length);
+
+    // Update best-overall-mean
+    if (meanDeltaTime < bestOverallMean) {
+        bestOverallMean = meanDeltaTime;
+        localStorage.setItem("best-overall-mean", JSON.stringify(bestOverallMean));
+        document.getElementById("best-overall-mean").innerText = bestOverallMean + "ms";
+    }
+
+    // Calculate standard deviation
+    let standardDeviation = 0.0;
+    for (let i = 0; i < reactionTimes.length; i++) {
+        let diff = reactionTimes[i] - meanDeltaTime;
+        standardDeviation += diff * diff;
+    }
+    standardDeviation = Math.round(Math.sqrt(standardDeviation / reactionTimes.length));
+
+    // Show results
+    let bestElement = document.createElement("p");
+    let worstElement = document.createElement("p");
+    let meanElement = document.createElement("p");
+    let sdElement = document.createElement("p");
+
+    bestElement.id = "best";
+    worstElement.id = "worst";
+    meanElement.id = "mean";
+    sdElement.id = "sd";
+
+    // Set fade-in animation
+    bestElement.className = "fadeInLeft";
+    worstElement.className = "fadeInLeft";
+    meanElement.className = "fadeInLeft";
+    sdElement.className = "fadeInLeft";
+
+    // Add resultsTitle
+    let resultsTitleElement = document.createElement("p");
+    resultsTitleElement.className = "resultsTitle";
+    resultsElement.appendChild(resultsTitleElement).innerText = "Results";
+
+    // Add all times
+    let allTimesWrapper = document.createElement("div");
+    resultsElement.appendChild(allTimesWrapper).id = "all-times-wrapper";
+
+    for (let i = 0; i < reactionTimes.length; i++) {
+        let time = document.createElement("p");
+        allTimesWrapper.appendChild(time).innerText = (i + 1) + ": " + reactionTimes[i] + "ms";
+    }
+
+    // Add calculated times
+    resultsElement.appendChild(bestElement).innerText = "Best: " + bestTime + "ms";
+    resultsElement.appendChild(worstElement).innerText = "Worst: " + worstTime + "ms";
+    console.log(reactionTimes);
+    resultsElement.appendChild(meanElement).innerText = "Mean: " + meanDeltaTime + "ms";
+    resultsElement.appendChild(sdElement).innerText = "Standard Deviation: " + standardDeviation + "ms";
+
+    // Create retry button
+    setTimeout(() => createRetryButton(), 1100);
+
+    function createRetryButton() {
+        let retryButtonElement = document.createElement("button");
+        retryButtonElement.className = "retryButton";
+        resultsElement.appendChild(retryButtonElement).innerText = "Retry test";
+        retryButtonElement.onclick = restartTest;
+    }
+}
+
+function restartTest() {
+    // Remove results
+    while (resultsElement.lastElementChild) {
+        resultsElement.removeChild(resultsElement.lastElementChild);
+    }
+
+    // Reset subtitle
+    subTitleElement.innerText = "Click the odd item to start";
+
+    // Create new initial grid
+    createRandomGrid(gridSize, gridSize);
 }
 
 function removePreviousGrid(row, col) {
@@ -76,6 +195,7 @@ function createRandomGrid(row, col) {
         // Mark the odd item
         if (i === oddItemIndex) {
             item.onclick = oddClicked;
+            item.className = "odd-item";
             itemContent.className += " odd-item-content-" + randomItemType;
         }
     }
@@ -83,6 +203,7 @@ function createRandomGrid(row, col) {
     // Hide random items for more random "grid"
     let itemsToHide = [];
     const numberOfItemsToHide = 8;
+
     while (itemsToHide.length < numberOfItemsToHide) {
         let randomIndex = Math.floor((row * col) * Math.random());
 
