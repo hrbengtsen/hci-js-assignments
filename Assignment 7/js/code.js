aa36bc896d20904d2f5e20bfbbd257e926e3a27e
hrbengtsen <mikkelbengtsen@gmail.com>
2020-11-09 10:35:13 +0100

    form 1 all done with timing and validation

--- Assignment 7/js/code.js
+++ Assignment 7/js/code.js
@@ -1,24 +1,57 @@
 const timerElement = document.getElementById("timer");
-const submitElement = document.getElementById("submit");
+const submitStateElement = document.getElementById("submitState");
+const forms = document.getElementsByClassName("needs-validation");
 
 let durationInterval;
+let duration = 0;
 
 timerElement.addEventListener("click", (event) => {
-  let duration = 0;
-  clearInterval(durationInterval);
+  if (duration !== 0) {
+    duration = 0;
+    clearInterval(durationInterval);
+    timerElement.innerHTML = "<i class='fas fa-stopwatch'></i> " + 0 + "s";
+  }
+  else {
+    durationInterval = setInterval(() => {
+      duration++;
+      timerElement.innerHTML = "<i class='fas fa-stopwatch'></i> " + formatTime(duration) + "s";
+    }, 1);
+  }
 
-  durationInterval = setInterval(() => {
-    duration++;
-    timerElement.innerHTML = "<i class='fas fa-stopwatch'></i> " + formatTime(duration) + "s";
-  }, 1);
+  Array.prototype.filter.call(forms, function(form) {
+    form.classList.remove('was-validated');
+    form.reset();
+  });
+  submitStateElement.innerText = "";
 });
 
-submitElement.addEventListener("click", (event) => {
-  event.preventDefault();
-  console.log("submitted");
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
 
-  clearInterval(durationInterval);
-});
+        if (duration === 0) {
+          submitStateElement.innerText = "Time not started.";
+        }
+        else {
+          if (form.checkValidity()) {
+            console.log("submitted");
+            clearInterval(durationInterval);
+            submitStateElement.innerText = "Form was submitted in " + formatTime(duration) + "s";
+          }
+          else {
+            submitStateElement.innerText = "Form was not validated.";
+          }
+          form.classList.add('was-validated');
+        }
+      }, false);
+    });
+  }, false);
+})();
 
 function formatTime(millis) {
   return (millis / 1000).toFixed(2);
